421b651fe290e210021da11356c11249c1487ead
Jake Thompson <wjakethompson@gmail.com>
2018-06-04 12:31:08 -0500

    initial commit

---
+++ R/hello.R
@@ -0,0 +1,26 @@
+#' An Exported function
+#'
+#' @export
+#' @examples
+#' hello()
+hello <- function() {
+  print("Hello, world!")
+}
+
+#' A Non-Exported Function
+goodbye <- function() {
+  print("Goodbye, world!")
+}
+
+#' Times Two Plus 5
+#'
+#' Multiplies an number by 2, then adds 5.
+#'
+#' @export
+#' @examples
+#' some_math(5)
+#' some_math(3)
+#' some_math(10)
+some_math <- function(x) {
+  timesTwo(x) + 5
+}
